cf28d0011d68d8d02e36267f4e6c3365a0242a00
K0311 <jckiminzw1212@gmail.com>
2025-04-11 13:07:47 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class BadlyNamedClass{
+public static void main(String args[]){
+
+int a[]={5,4,3,2,1};bubblebadSort(a);
+for(int i=0;i<a.length;i++){System.out.print(a[i]+" ");}
+
+}
+static void bubblebadSort(int numz[]){
+for(int i=0;i<numz.length-1;i++)
+{
+for(int j=0;j<numz.length-i;j++){
+if(numz[j]>numz[j+1]){
+int temp=num
